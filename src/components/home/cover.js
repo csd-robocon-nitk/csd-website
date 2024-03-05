23b1e7be762e88a88ab419734b2f98e5e75bec03
Hariharan <jhariharan2004@gmail.com>
2024-03-05 18:41:45 +0530

    Added cover section in homepage

--- src/components/home/cover.js
+++ src/components/home/cover.js
@@ -1,11 +1,12 @@
 export default function CoverSection () {
     return (
-        <div className="">
-            <video autoPlay muted loop>
+        <div className="hero h-screen overflow-hidden">
+            <div className="hero-overlay z-10 bg-opacity-90"></div>
+            <video autoPlay muted loop className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full object-cover">
                 <source src="/cover_video.mp4" />
             </video>
-            <div className="">
-                <h1>Center for System Design <br /> NITK</h1>
+            <div className="hero-content z-10">
+                <h1 className="text-5xl font-bold text-center leading-normal">Center for System Design <br /> NITK</h1>
             </div>
         </div>
     )
