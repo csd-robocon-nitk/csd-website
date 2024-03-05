a023d7e850e0ea7041975dac3b7008ec85886e1d
Hariharan <jhariharan2004@gmail.com>
2024-03-06 01:04:00 +0530

    Added header with navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,17 @@
+import Link from "next/link"
+
 export default function Header () {
-    return (<></>)
+    return (
+        <div className="z-50 fixed w-full h-20 top-0 bg-sky-600 flex items-center justify-between px-7">
+            <img src="/logo.webp" alt="" />
+            <ul className="flex gap-16 text-md font-bold text-indigo-950 uppercase">
+                <li><Link href = "/" >Home</Link></li>
+                <li><Link href = "/people" >People</Link></li>
+                <li><Link href = "/blog" >Blog</Link></li>
+                <li><Link href = "/resources" >Resources</Link></li>
+                <li><Link href = "/events" >Events</Link></li>
+                <li><Link href = "/contact" >Contact</Link></li>
+            </ul>
+        </div>
+    )
 }
--- src/components/home/cover.js
+++ src/components/home/cover.js
@@ -1,12 +1,11 @@
 export default function CoverSection () {
     return (
-        <div className="hero h-screen overflow-hidden">
-            <div className="hero-overlay z-10 bg-opacity-90"></div>
-            <video autoPlay muted loop className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full object-cover">
+        <div className="h-screen overflow-hidden">
+            <video autoPlay muted loop className="absolute top-0 left-0 w-full h-full object-cover">
                 <source src="/cover_video.mp4" />
             </video>
-            <div className="hero-content z-10">
-                <h1 className="text-6xl font-black text-center leading-tight text-slate-300 drop-shadow-2xl shadow-black cover-heading">CENTER FOR SYSTEM DESIGN<br />NITK</h1>
+            <div className="absolute top-0 left-0 flex justify-center items-center bg-zinc-900/85 w-full h-full">
+                <h1 className="text-6xl font-black text-center leading-tight text-zinc-300 drop-shadow-2xl shadow-black cover-heading">CENTER FOR SYSTEM DESIGN<br />NITK</h1>
             </div>
         </div>
     )
