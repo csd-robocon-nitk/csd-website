72451b2ea8e8ed0a756bdc7dcde6aa1c3874278d
Hariharan <jhariharan2004@gmail.com>
2024-03-06 20:34:24 +0530

    Updated header with new logo and ham, changed bg color

--- src/components/header.js
+++ src/components/header.js
@@ -2,16 +2,17 @@ import Link from "next/link"
 
 export default function Header () {
     return (
-        <div className="z-50 fixed w-full h-20 top-0 bg-sky-600 flex items-center justify-between px-7">
-            <img src="/logo.webp" alt="" />
-            <ul className="flex gap-16 text-md font-bold text-indigo-950 uppercase">
+        <div className="z-50 fixed w-full h-20 top-0 bg-gradient-to-r from-neutral-600 to-neutral-800 flex items-center justify-between gap-6 px-7 shadow shadow-black/30">
+            <img src="/logo.png" className="h-16" alt="" />
+            {/* <ul className="flex gap-16 text-md font-bold text-white uppercase">
                 <li><Link href = "/" >Home</Link></li>
                 <li><Link href = "/people" >People</Link></li>
                 <li><Link href = "/blog" >Blog</Link></li>
                 <li><Link href = "/resources" >Resources</Link></li>
                 <li><Link href = "/events" >Events</Link></li>
                 <li><Link href = "/contact" >Contact</Link></li>
-            </ul>
+            </ul> */}
+            <img src="/ham.svg" className="invert h-12 cursor-pointer" alt="" />
         </div>
     )
 }
--- src/components/home/cover.js
+++ src/components/home/cover.js
@@ -4,7 +4,7 @@ export default function CoverSection () {
             <video autoPlay muted loop className="absolute top-0 left-0 w-full h-full object-cover">
                 <source src="/cover_video.mp4" />
             </video>
-            <div className="absolute top-0 left-0 flex justify-center items-center bg-zinc-900/85 w-full h-full">
+            <div className="absolute top-0 left-0 flex justify-center items-center bg-zinc-900/75 w-full h-full">
                 <h1 className="text-6xl font-black text-center leading-tight text-zinc-300 drop-shadow-2xl shadow-black cover-heading">CENTER FOR SYSTEM DESIGN<br />NITK</h1>
             </div>
         </div>
