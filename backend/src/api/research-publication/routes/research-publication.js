69844712f7e8e1722594fe0a797d249690440195
Hariharan <jhariharan2004@gmail.com>
2024-12-16 16:47:31 +0530

    Added testimonial and research publications schema in backend

---
+++ backend/src/api/research-publication/routes/research-publication.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * research-publication router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::research-publication.research-publication');
