50fdcaf16eb18ba4848d7aed065c998e0d92011c
Shanjiv A <shanjv177@gmail.com>
2024-12-20 11:31:04 +0530

    added sponsors content

---
+++ src/components/home/sponsors/page.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Scroll } from "./sponsors";
+
+async function Sponsors() {
+  const token = process.env.NEXT_PUBLIC_TOKEN;
+  if (!token) {
+    throw new Error("Token not found!");
+  }
+
+  let logos = await fetch(
+    `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/sponsors?populate=*`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      cache: "no-cache",
+    }
+  );
+
+  if (!logos.ok) {
+    throw new Error(
+      `Failed to fetch data: ${logos.status} ${logos.statusText}`
+    );
+  }
+
+  logos = await logos.json()
+  console.log(logos)
+
+  return (
+    <div className="flex flex-col mb-12 justify-center items-center gap-8">
+      <h1 className="text-4xl font-bold sticky top-16 pt-12">Sponsors</h1>
+      <div className="h-fit flex flex-col antialiased items-center justify-center relative overflow-hidden w-11/12 mx-auto">
+        <Scroll items={logos.data} direction="left" speed="slow" />
+      </div>
+    </div>
+  );
+}
+
+export default Sponsors;
---
+++ src/components/home/sponsors/sponsors.js
@@ -0,0 +1,89 @@
+"use client";
+import { cn } from "@/components/home/utils";
+import React, { useEffect, useState } from "react";
+import './sponsors.css'
+
+export const Scroll = ({
+  items,
+  direction = "left",
+  speed = "fast",
+  pauseOnHover = true,
+  className,
+}) => {
+  const containerRef = React.useRef(null);
+  const scrollerRef = React.useRef(null);
+
+  useEffect(() => {
+    addAnimation();
+  }, []);
+  const [start, setStart] = useState(false);
+  function addAnimation() {
+    if (containerRef.current && scrollerRef.current) {
+      const scrollerContent = Array.from(scrollerRef.current.children);
+
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        if (scrollerRef.current) {
+          scrollerRef.current.appendChild(duplicatedItem);
+        }
+      });
+
+      getDirection();
+      getSpeed();
+      setStart(true);
+    }
+  }
+  const getDirection = () => {
+    if (containerRef.current) {
+      if (direction === "left") {
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "forwards"
+        );
+      } else {
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "reverse"
+        );
+      }
+    }
+  };
+  const getSpeed = () => {
+    if (containerRef.current) {
+      if (speed === "fast") {
+        containerRef.current.style.setProperty("--animation-duration", "20s");
+      } else if (speed === "normal") {
+        containerRef.current.style.setProperty("--animation-duration", "40s");
+      } else {
+        containerRef.current.style.setProperty("--animation-duration", "80s");
+      }
+    }
+  };
+  return (
+    <div
+      ref={containerRef}
+      className={cn(
+        "scroller relative z-20  max-w-7xl overflow-hidden  [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+        className
+      )}
+    >
+      <ul
+        ref={scrollerRef}
+        className={cn(
+          "flex min-w-full shrink-0 gap-4 py-4 w-max flex-nowrap items-center",
+          start && "animate-scroll ",
+          pauseOnHover && "hover:[animation-play-state:paused]"
+        )}
+      >
+        {items.map((logo, idx) => (
+          <img
+            key={idx}
+            src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/${logo.attributes.logo.data.attributes.url}`}
+            className="h-20 px-2"
+            alt={`logo-${logo}`}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
