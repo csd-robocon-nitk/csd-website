5f359a68c73f527aba3fd87c6939f941d88fd348
Shanjiv A <shanjv177@gmail.com>
2024-12-24 12:29:55 +0530

    fixes in header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -42,7 +42,7 @@ export default function Header () {
 
     return (
 		<TransparencyContext.Provider value={navTransparency}>
-			<div className={`fixed top-0 h-20 w-full z-50 duration-300 ${!navTransparency || open ? "bg-white backdrop-blur-md drop-shadow-lg" : ""}`}>
+			<div className={`fixed top-0 h-20 w-full z-[999] duration-300 ${!navTransparency || open ? "bg-white backdrop-blur-md drop-shadow-lg" : ""}`}>
 				<div className="py-3 px-6 flex flex-wrap items-center lg:gap-4 lg:justify-evenly justify-between w-full">
 					<Link href="/">
 						<img src="/logo.png" className={`md:h-14 h-10 ${!navTransparency || open ? "invert" : ""} duration-300`} alt="" />
