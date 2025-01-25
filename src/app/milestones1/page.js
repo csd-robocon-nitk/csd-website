95c264d7237d0de8a1651f93aa99300e3973d05c
Shanjiv A <shanjv177@gmail.com>
2025-01-25 10:53:38 +0530

    milestones revamp

--- src/app/milestones1/page.js
+++ src/app/milestones1/page.js
@@ -2,8 +2,9 @@ import Image from "next/image";
 import React from "react";
 import { Timeline } from "./timeline";
 import Splash from "@/components/splash";
+import HorizontalTimeline from "./horizontalTimeline";
 
-export default async function TimelineDemo() {
+export default async function TimelinePage() {
   const token = process.env.NEXT_PUBLIC_TOKEN;
   if (!token) {
     throw new Error("Token not found!");
@@ -25,34 +26,23 @@ export default async function TimelineDemo() {
   const data1 = await res.json();
   const milestones = await data1.data;
 
-  {
-    /* 
-    
-    Title of the card should be milestone.timeline
-
-    body should be 
-    h1 milstone.title
-    then a paragraph of milestone.description
-
-    */
-  }
-
   const data = milestones.map((milestone) => ({
     title: milestone.attributes.timeline,
     content: (
       <div>
-        <h1 className="text-3xl font-bold mb-2">
+        <h1 className="text-3xl font-bold text-wrap">
           {milestone.attributes.title}
         </h1>
-        <div className="text-base">{milestone.attributes.description}</div>
+        <div className="text-base text-wrap">{milestone.attributes.description}</div>
       </div>
     ),
-  }));
+  })).sort((a, b) => a.title - b.title);
 
   return (
     <div className="w-full">
       <Splash title={"Our Milestones"} subtitle={"Trace our journey through key moments and achievement"} />
       <Timeline data={data} />
+      <HorizontalTimeline className="" events={data} />
     </div>
   );
 }
--- src/app/milestones1/timeline.js
+++ src/app/milestones1/timeline.js
@@ -3,42 +3,32 @@ import { useScroll, useTransform, motion } from "framer-motion";
 import React, { useEffect, useRef, useState } from "react";
 
 export const Timeline = ({ data }) => {
-  const containerRef = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: containerRef,
-    offset: ["start end", "end start"]
-  });
+
+  const length = data.length;
 
   return (
-    <div ref={containerRef} className="relative flex flex-col items-center">
-      {data.map((item, index) => (
-        <div key={index} className="flex w-full mb-10">
-          {/* Left Side Content */}
-          <div className="w-1/2 pr-4">
-            <motion.div
-              initial={{ opacity: 0, y: 50 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5, delay: index * 0.1 }}
-              viewport={{ once: true }}
-            >
-              {item.content}
-            </motion.div>
-          </div>
+    <div className="hidden relative lg:flex flex-col items-center no-scrollbar px-32 overflow-scroll h-[75vh] pt-10">
+      <div className="sticky top-0 translate-x-[-225px] text-8xl font-bold">
+        20
+      </div>
+      <div className="absolute top-10 max-w-[600px] translate-x-[50px]">
+        {data.map((item, index) => (
+          <div key={index} className="flex justify-center gap-16">
+            {/* Left */}
 
-          {/* Right Side Year */}
-          <div className="w-1/2 pl-4 flex justify-end items-center">
-            <motion.div
-              initial={{ opacity: 0, y: 50 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5, delay: index * 0.1 }}
-              viewport={{ once: true }}
-              className="text-2xl font-bold"
-            >
-              {item.title}
-            </motion.div>
+            <div className="relative text-8xl font-bold w-1/3 justify-center items-center text-center h-[50vh]">
+              <h1 className="absolute top-0 left-1/2 bg-sky-50 z-[10] p-0 m-0">
+                {item.title.slice(-2)}
+              </h1>
+              { !(length == (index+1)) && <div className="absolute z-0 w-[4px] bg-sky-950 left-1/2 h-full translate-x-[60px]">
+              </div>}
+            </div>
+
+            {/* Right */}
+            <div className="w-2/3 text-wrap pt-12 px-12">{item.content}</div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
