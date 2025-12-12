7f5b6714e6a2bef4f3e7da16978f5781b6ad8a01
adarsh <adarshbellamane@gmail.com>
2025-12-12 15:59:32 +0530

    homepage-strapi-update

---
+++ backend/src/api/home-page-about-us/controllers/home-page-about-us.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-page-about-us controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::home-page-about-us.home-page-about-us');
